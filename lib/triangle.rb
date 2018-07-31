23aa497f52fb65fa3ab515b6ce5d4ede1d783223
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-31 19:52:54 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,12 +13,8 @@ def kind
   elsif side_1 != side_2 && side_2 != side_3 && side_1 != side_3
   return :scalene
 elsif side_1 <= 0 || side_2 <=0 || side_3 <=0  
-  begin
         raise TriangleError
-      rescue TriangleError=> error
-          puts error.message
       end
-end
 end
 class TriangleError < StandardError
   def message
