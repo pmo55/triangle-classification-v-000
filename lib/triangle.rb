5331c42a98c50c909e6eced176ad85806f46ca96
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-31 14:07:35 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,6 +10,8 @@ def kind
     return :equilateral
   elsif side_1==side_2 || side_2==side_3 || side_1=side_3
   return :isosceles
+  elsif side_1 != side_2 && side_2 != side_3
+  return :scalene
 end
 end
 end
